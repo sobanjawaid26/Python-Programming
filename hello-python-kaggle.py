21c0817e973cbc3e4c6aaef8722ad9e4bf4d4595
sobanjawaid30 <sobanjawaid30@gmail.com>
2019-10-09 14:17:46 +0530

    hello python completed from kaggle

---
+++ hello-python-kaggle.py
@@ -0,0 +1,19 @@
+spam_amount = 0
+
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+
+spam_amount = spam_amount + 4
+
+print(spam_amount)
+
+if spam_amount > 0:
+	print('But I dont want any spam!')
+
+viking_message = 'SPAM SPAM SPAM'
+
+print(viking_message)
+
+## print(type(spam_amount))
+## print(type(viking_message))
